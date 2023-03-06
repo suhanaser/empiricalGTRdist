604ad0e7bec1d0f242c83941853f1e9c51266ff8
suhanaser <152suha@gmail.com>
2023-03-06 08:31:01 +0100

    Update extract_brach_lengths_dist_from_empirical_data.py

--- extract_brach_lengths_dist_from_empirical_data.py
+++ extract_brach_lengths_dist_from_empirical_data.py
@@ -16,33 +16,48 @@ def branchLen(treefile):
     This function works with tree files in newick format 
     '''
     t = Tree(treefile)
-    branchLenDist = []
+    branchLenDist_leaf = []
+    branchLenDist_int = []
     for n in t.traverse():
         if n.dist > 0:
-            branchLenDist.append(n.dist)
-    df = pd.DataFrame({'branchLen':branchLenDist})
-    df['dataset'] = os.path.basename(os.path.dirname(treefile))
-    return df
+            if n.is_leaf():
+                branchLenDist_leaf.append(n.dist)
+            else:
+                branchLenDist_int.append(n.dist)
+    df_leaf = pd.DataFrame({'branchLen':branchLenDist_leaf})
+    df_leaf['dataset'] = os.path.basename(os.path.dirname(treefile))
+    df_int = pd.DataFrame({'branchLen':branchLenDist_int})
+    df_int['dataset'] = os.path.basename(os.path.dirname(treefile))
+    return df_leaf, df_int
 
 
 if __name__ == '__main__':
-    print("In order to run this script all files must have the same name and extension and they should be saved in directories that have the datasets name. Please see an example below")
-    diagram = Tree("((----->treeFileName.treefile)----->dataset1Dir, (----->treeFileName.treefile)----->dataset2Dir, (----->treeFileName.treefile)----->dataset3Dir)rootDir;", format=1)
+    print("In order to run this script all files must have the same name and extension"\
+          " and they should be saved in directories that have the datasets name"\
+              " Please see an example below")
+    diagram = Tree("((----->treeFileName.treefile)----->dataset1Dir, "\
+                   "(----->treeFileName.treefile)----->dataset2Dir, "\
+                       "(----->treeFileName.treefile)----->dataset3Dir)rootDir;"
+                       , format=1)
     print(diagram.get_ascii(show_internal=True))
-    rootDir = '/data/Suha/GTR_parameters_dist' #the rootDir name to the directories that contain the tree files
-    treeFileName = 'branches.treefile' #the name of the tree file with .treefile extension (any newick format file can be used)
-    branchLenFile = 'BranchLen.csv' #the name of the branch lengths output file with .csv extension
+    rootDir = '/MY_PATH/' #the root directory that contain the tree files
+    treeFileName = 't.treefile' #the name of the tree file with .treefile extension 
+    branchLenFile_leaf = 'BranchLen_leaf.csv' #the name of the leaves branch lengths output file
+    branchLenFile_int = 'BranchLen_int.csv' #the name of the internal branch lengths output file 
     proceed = input("do you want to proceed? Y/N\n")
 
     if proceed == 'Y':
     
-        df = pd.DataFrame()
+        df_leaf = pd.DataFrame()
+        df_int = pd.DataFrame()
     
         for DirName, subdirList, fileList in os.walk(rootDir):
             if treeFileName in fileList:
                 treeFile = os.path.join(DirName,treeFileName)
-                df = df.append(branchLen(treeFile))
+                df_leaf = df_leaf.append(branchLen(treeFile)[0])
+                df_int = df_int.append(branchLen(treeFile)[1])
     
-        df.to_csv(os.path.join(rootDir, branchLenFile))
+        df_leaf.to_csv(os.path.join(rootDir, branchLenFile_leaf))
+        df_int.to_csv(os.path.join(rootDir, branchLenFile_int))
     else:
         sys.exit()
